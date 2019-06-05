cabd15c414ea969c898de4a77e3d6660df49c406
fbrevers <f.brevers.gomez@accenture.com>
2019-06-05 10:36:56 +0200

    Cambios esteticos.

--- faces.py
+++ faces.py
@@ -29,9 +29,8 @@ def detect_export_faces(image):
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
         roi_color = image[y:y + h, x:x + w]
-        print("[Analisis] Posible sospechoso.")
-        cv2.imwrite(OUTPUT_FOLDER + str(w) + str(h) + '_faces.jpg', roi_color)
-        print(OUTPUT_FOLDER + str(w) + str(h) + '_faces.jpg')
+        cv2.imwrite(OUTPUT_FOLDER + 'sospechoso_' + str(w) + str(h) + '.jpg', roi_color)
+        print('[Analisis] Posible sospechoso: sospechoso_' + str(w) + str(h) + '.jpg')
 
     cv2.imwrite('faces_detected.jpg', image)
 
